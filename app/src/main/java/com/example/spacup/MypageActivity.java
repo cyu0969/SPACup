c9dd8d1ceb58fbcf6083a5cafdeb3410e9986d74
cyu0969 <dyddnr5376@gmail.com>
2019-05-18 04:00:34 +0900

    마이페이지 기능 완성

--- app/src/main/java/com/example/spacup/MypageActivity.java
+++ app/src/main/java/com/example/spacup/MypageActivity.java
@@ -3,11 +3,15 @@ package com.example.spacup;
 import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -15,13 +19,20 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.spacup.item.MemberInfoItem;
+import com.example.spacup.lib.EtcLib;
 import com.example.spacup.lib.MyLog;
+import com.example.spacup.lib.MyToast;
 import com.example.spacup.lib.StringLib;
 import com.example.spacup.remote.RemoteService;
+import com.example.spacup.remote.ServiceGenerator;
 import com.squareup.picasso.Picasso;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MypageActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -90,7 +101,7 @@ public class MypageActivity extends AppCompatActivity implements View.OnClickLis
         sextypeEdit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                setSexTypeDialog();
+                setEducationTypeDialog();
             }
         });
 
@@ -104,23 +115,26 @@ public class MypageActivity extends AppCompatActivity implements View.OnClickLis
         });
     }
 
-    private void setSexTypeDialog() {
-        final String[] sexTypes = new String[2];
-        sexTypes[0] = getResources().getString(R.string.sex_man);
-        sexTypes[1] = getResources().getString(R.string.sex_woman);
+    // 학력을 선택할 수 있는 다이얼로그 출력
+    private void setEducationTypeDialog() {
+        final String[] educationTypes = new String[3];
+        educationTypes[0] = getResources().getString(R.string.education_high);
+        educationTypes[1] = getResources().getString(R.string.education_univ);
+        educationTypes[2] = getResources().getString(R.string.education_grad);
 
         new AlertDialog.Builder(this)
-                .setItems(sexTypes, new DialogInterface.OnClickListener() {
+                .setItems(educationTypes, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         if (which >= 0) {
-                            sextypeEdit.setText(sexTypes[which]);
+                            sextypeEdit.setText(educationTypes[which]);
                         }
                         dialog.dismiss();
                     }
                 }).show();
     }
 
+    // 생일을 선택할 수 있는 다이얼로그 출력
     private void setBirthdayDialog() {
         GregorianCalendar calendar = new GregorianCalendar();
         int year = calendar.get(Calendar.YEAR);
@@ -150,9 +164,149 @@ public class MypageActivity extends AppCompatActivity implements View.OnClickLis
         }, year, month, day).show();
     }
 
+    // 오른쪽 상단 메뉴 구성
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_submit, menu);
+        return true;
+    }
+
+    // 왼쪽 상단의 아이콘으로 뒤로 가기 및 저장 기능을 이용하기 위한 메소드
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                close();
+                break;
+
+            case R.id.action_submit:
+                save();
+                break;
+        }
+
+        return true;
+    }
+
+    // 사용자가 수정하면 내용을 사용자 정보에 저장하는 메소드
+    private MemberInfoItem getMemberInfoItem() {
+        MemberInfoItem item = new MemberInfoItem();
+        item.phone = EtcLib.getInstance().getPhoneNumber(context);
+        item.name = nameEdit.getText().toString();
+        item.sextype = sextypeEdit.getText().toString();
+        item.birthday = birthEdit.getText().toString().replace(" ", "");
+
+        return item;
+    }
+
+    // 기존 사용자 정보랑 이전 사용자 정보랑 비교하는 메소드
+    // 변경되었으면 참, 아니면 거짓.
+    private boolean isChanged(MemberInfoItem newItem) {
+        if (newItem.name.trim().equals(currentItem.name)
+                && newItem.sextype.trim().equals(currentItem.sextype)
+                && newItem.birthday.trim().equals(currentItem.birthday)) {
+            Log.d(TAG, "return " + false);
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // 사용자가 이름을 변경했는지 확인
+    // 변경되었으면 참, 아니면 거짓.
+    private boolean isNoName(MemberInfoItem newItem) {
+        if (StringLib.getInstance().isBlank(newItem.name)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // 사용자가 변경한 내용을 저장하는 메소드
+    private void save() {
+        final MemberInfoItem newItem = getMemberInfoItem();
+
+        if (!isChanged(newItem)) {
+            MyToast.s(this, R.string.no_change);
+            finish();
+            return;
+        }
+
+        MyLog.d(TAG, "insertItem " + newItem.toString());
+
+        RemoteService remoteService =
+                ServiceGenerator.createService(RemoteService.class);
+
+        Call<String> call = remoteService.insertMemberInfo(newItem);
+        call.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                if (response.isSuccessful()) {
+                    String seq = response.body();
+                    try {
+                        currentItem.seq = Integer.parseInt(seq);
+                        if (currentItem.seq == 0) {
+                            MyToast.s(context, R.string.member_insert_fail_message);
+                            return;
+                        }
+                    } catch (Exception e) {
+                        MyToast.s(context, R.string.member_insert_fail_message);
+                        return;
+                    }
+                    currentItem.name = newItem.name;
+                    currentItem.sextype = newItem.sextype;
+                    currentItem.birthday = newItem.birthday;
+                    finish();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+            }
+        });
+    }
+
+    // 변경사항 없으면 뒤로가기 있으면 변경사항 자동 저장
+    private void close() {
+        MemberInfoItem newItem = getMemberInfoItem();
+
+        if (!isChanged(newItem) && !isNoName(newItem)) {
+            finish();
+        } else if (isNoName(newItem)) {
+            MyToast.s(context, R.string.name_need);
+            finish();
+        } else {
+            new AlertDialog.Builder(this).setTitle(R.string.change_save)
+                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            save();
+                        }
+                    })
+                    .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    })
+                    .show();
+        }
+    }
+
+    // 뒤로가기 버튼을 클릭할 경우 close메소드 불러옴
+    @Override
+    public void onBackPressed() {
+        close();
+    }
 
     @Override
     public void onClick(View v) {
+        if (v.getId() == R.id.profile_icon || v.getId() == R.id.profile_icon_change) {
+            startProfileIconChange();
+        }
+    }
 
+    private void startProfileIconChange() {
+        Intent intent = new Intent(this, MypageIconActivity.class);
+        startActivity(intent);
     }
 }
